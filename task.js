62927e2b1ec78e1fe6e95706c863667372582fe8
kowsalyaj2007 <kowsalyaj2007@gmail.com>
2026-03-09 19:35:10 +0530

    Fix height style of element 'a' in task.js

--- task.js
+++ task.js
@@ -5,12 +5,11 @@ b.style.color="white";
 console.log(b);
 
 let a = document.getElementById("div");
-a.style.textAlign="center";
 a.style.backgroundColor = "rgb(95, 42, 23)";
 a.style.borderRadius="5px";
 a.style.margin = "10px";
 a.style.width="300px";
-a.style.height="350px";
+a.style.height="300px";
 console.log(a);
 
 let k=document.getElementById("p1")
